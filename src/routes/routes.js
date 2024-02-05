7200d3d5040f683abead170660f3da32b911f2fa
Alsje <alsje@mernok.co.za>
2024-02-05 22:43:52 +0100

    feat: 🎸 authentication; JWT & Passport Strategy
    Set up for secure authentication complete, need to refactor error
    handling and adapt according to different user journies eg. log in with
    email

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,4 +4,9 @@ module.exports.attachRoutes = (router) => {
 	bindCustomRegistrationToRouter(router)
 
 	router.registerRoute(require('./user/post.route').post)
+	router.registerRoute(require('./auth/register.route').post)
+	router.registerRoute(require('./auth/login.route').post)
+	router.registerRoute(require('./auth/token.route').post)
+	router.registerRoute(require('./auth/protected.route').get)
+	router.registerRoute(require('./auth/logout.route').post)
 }
